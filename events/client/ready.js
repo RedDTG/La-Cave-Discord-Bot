b317b6e5274a596be9765c173259f3c1cb54eed2
Antoine <a.deyris85@gmail.com>
2022-08-29 18:38:43 +0200

    - into _

--- events/client/ready.js
+++ events/client/ready.js
@@ -7,8 +7,8 @@ module.exports = {
     async execute(client) {
         console.log('Je suis prêt !');
 
-        const devGuild = await client.guilds.cache.get(process.env.DEV-GUILD);
-        const prdGuild = await client.guilds.cache.get(process.env.PRD-GUILD);
+        const devGuild = await client.guilds.cache.get(process.env.DEV_GUILD);
+        const prdGuild = await client.guilds.cache.get(process.env.PRD_GUILD);
         devGuild.commands.set(client.commands.map(cmd => cmd));
         prdGuild.commands.set(client.commands.map(cmd => cmd));
     }
