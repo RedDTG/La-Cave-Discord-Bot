89a63e3f38ee07e318f45e32e0ec1e6b11ce17ce
Mr-R1 <56195379+EL-R1@users.noreply.github.com>
2022-12-22 01:40:14 +0100

    Notification check

--- buttons/animes/notificationButton.js
+++ buttons/animes/notificationButton.js
@@ -8,21 +8,25 @@ module.exports = {
     permissions: [],
     runInteraction(client, interaction) {
 
-        
+
         const value = Object.values(databases.animes).find(o => o.message_id === interaction.message.id);
-        notif = databases.notifications[value.id];
-        
-        let content="";
-        if(notif.includes(interaction.user.id))
-        {
-            notif.pop();
-            content=`Tu as enlevé **\`${value.title}\`** de ta liste - **\`/list\`** pour voir tous tes animes`
-        }else{
-            notif.push(interaction.user.id);
-            content=`Tu as ajouté **\`${value.title}\`** à ta liste de notification`
+        notif = databases.notifications;
+
+        let content = "";
+
+        const obj = notif.find(obj => Object.keys(obj)[0] === String(value.id));
+
+        if (obj) {
+            const exists = obj[String(value.id)].includes(interaction.user.id);
+
+            if (exists) {
+                obj[String(value.id)].splice(obj[value.id].indexOf(interaction.user.id), 1);
+                content = `Tu as enlevé **\`${value.title}\`** de ta liste - **\`/list\`** pour voir tous tes animes`
+            } else {
+                obj[String(value.id)].push(interaction.user.id);
+                content = `Tu as ajouté **\`${value.title}\`** à ta liste de notification`
+            }
         }
-        
-        
 
         writeFile("data/notifications.json", JSON.stringify(databases.notifications), (err) => { if (err) { console.log(err) } });
         return interaction.reply({ content: content, ephemeral: true })
