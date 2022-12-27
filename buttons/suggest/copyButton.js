7daaeffc61b694feb5615b33b04dcb79144dabd5
Mr-R1 <56195379+EL-R1@users.noreply.github.com>
2022-12-27 20:13:12 +0100

    Delete copyButton.js

--- buttons/suggest/copyButton.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'copy-button',
-    permissions: [],
-    runInteraction(client, interaction) {
-        clipboard.writeSync('Hello !')
-        interaction.reply({content: 'Contenu copié !', ephemeral: true})
-    }
-};
